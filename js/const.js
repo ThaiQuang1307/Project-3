8a89db2650194c4ef204876c9250c062bfbb9400
ThaiQuang1307 <quangmath1307@gmail.com>
2023-04-25 17:53:09 +0700

    ....

--- js/const.js
+++ js/const.js
@@ -1,4 +1,5 @@
-const PATH = "http://localhost:3000";
+// const PATH = "http://localhost:3000";
+const PATH = "https://json-server-quang.vercel.app";
 
 export const API_PRODUCT = `${PATH}/products`;
 export const API_CATEGORY = `${PATH}/category`;
